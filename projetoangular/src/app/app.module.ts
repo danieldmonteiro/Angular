53ba99fe07b3d012fab4b790e7604b64b102cfec
danieldmonteiro <danieldmonteiro@gmail.com>
2022-02-25 20:55:16 -0300

    quarto commit angular

--- projetoangular/src/app/app.module.ts
+++ projetoangular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +7,20 @@ import { AppComponent } from './app.component';
 import { ContatComponent } from './views/contat/contat.component';
 import { JogadorComponent } from './views/jogador/jogador.component';
 import { HomeComponent } from './views/home/home.component';
+import { ModelsComponent } from './views/models/models.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContatComponent,
     JogadorComponent,
-    HomeComponent
+    HomeComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
